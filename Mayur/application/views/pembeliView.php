66be1b3d069f1b566de8027a8d9e94656792b09b
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-11-25 13:45:37 +0700

    Update Templating View

--- Mayur/application/views/pembeliView.php
+++ Mayur/application/views/pembeliView.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/penjual.css"> 
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/penjual.css">
 	<title>Welcome Page</title>
 </head>
+
 <body>
-	<img src="<?php echo base_url();?>assets/img/sayur.jpg" class="backgroundImage">
+	<img src="<?php echo base_url(); ?>assets/img/sayur.jpg" class="backgroundImage">
 	<form class="box" action="proses_login.php" method="POST">
-			<h1 class="h1"><b>Pembeli</b></h1>
-			<img src="<?php echo base_url();?>assets/img/pembeli.png" class="imageBox">
-			<h3 class="h1"><b>Selamat datang di Mayur!</b></h3>
-			<h5 class="h1"><b>Sudah siap untuk beli bahan masakan?</b></h5>
-			<!-- <input type="text" name="username" placeholder="Username">
+		<h1 class="h1"><b>Pembeli</b></h1>
+		<img src="<?php echo base_url(); ?>assets/img/pembeli.png" class="imageBox">
+		<h3 class="h1"><b>Selamat datang di Mayur!</b></h3>
+		<h5 class="h1"><b>Sudah siap untuk beli bahan masakan?</b></h5>
+		<!-- <input type="text" name="username" placeholder="Username">
 			<input type="password" name="password" placeholder="Password"> -->
-			<input type="submit" name="login" value="Daftar">
-			<input type="submit" name="login" value="Login">
+		<input type="submit" name="login" value="Daftar">
+		<input type="submit" name="login" value="Login">
 	</form>
 </body>
-</html>
 
+</html>
--- Mayur/application/views/welcomeView.php
+++ Mayur/application/views/welcomeView.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/welcome.css"> 
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/welcome.css">
 	<title>Welcome Page</title>
 </head>
+
 <body>
-	<img src="<?php echo base_url();?>assets/img/sayur.jpg" class="backgroundImage">
-	<form class="box" action="proses_login.php" method="POST">
-			<h1 class="h1"><b>Mayur</b></h1>
-			<img src="<?php echo base_url();?>assets/img/background.png" class="imageBox">
-			<h3 class="h1"><b>Yuk masak sendiri!</b></h3>
-			<!-- <input type="text" name="username" placeholder="Username">
+	<img src="<?php echo base_url(); ?>assets/img/sayur.jpg" class="backgroundImage">
+	<form class="box" action="<?php echo base_url(); ?>chooserController" method="POST">
+		<h1 class="h1"><b>Mayur</b></h1>
+		<img src="<?php echo base_url(); ?>assets/img/background.png" class="imageBox">
+		<h3 class="h1"><b>Yuk masak sendiri!</b></h3>
+		<!-- <input type="text" name="username" placeholder="Username">
 			<input type="password" name="password" placeholder="Password"> -->
-			<input type="submit" name="login" value="Mulai Aplikasi">
+		<input type="submit" name="login" value="Mulai Aplikasi">
 	</form>
 </body>
-</html>
 
+</html>
